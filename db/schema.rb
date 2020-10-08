18e99037c91c384dc93905832e09dedd432cb5c3
Longxun Jin <longxjca@gmail.com>
2020-10-08 00:03:23 -0500

    ran migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_08_044518) do
+ActiveRecord::Schema.define(version: 2020_10_08_050030) do
+
+  create_table "planets", force: :cascade do |t|
+    t.string "name"
+    t.string "gravity"
+    t.string "population"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "species", force: :cascade do |t|
     t.string "name"
