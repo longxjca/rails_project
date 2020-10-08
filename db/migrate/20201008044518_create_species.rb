c302cd5a5c0699f788b637c3eeb6ebd5dd696e6c
Longxun Jin <longxjca@gmail.com>
2020-10-07 23:45:43 -0500

    added a new model Species

---
+++ db/migrate/20201008044518_create_species.rb
@@ -0,0 +1,11 @@
+class CreateSpecies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :species do |t|
+      t.string :name
+      t.string :classification
+      t.string :language
+
+      t.timestamps
+    end
+  end
+end
