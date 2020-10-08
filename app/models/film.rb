f6ab62f6f7a5aaa58c33f8dda9f4790896b2a8bb
Longxun Jin <longxjca@gmail.com>
2020-10-08 01:23:09 -0500

    added associations for planet, film models

--- app/models/film.rb
+++ app/models/film.rb
@@ -2,6 +2,9 @@ class Film < ApplicationRecord
   has_many :person_films
   has_many :people, through: :person_films
 
+  has_many :film_planets
+  has_many :planets, through: :film_planets
+
   validates :title, uniqueness: true
   validates :title, presence: true
 end
--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,5 +1,9 @@
 class Planet < ApplicationRecord
-  has_many :planets
+  has_many :people
+
+  has_many :film_planets
+  has_many :films, through: :film_planets
+
   validates :name, uniqueness: true
   validates :name, presence: true
 end
