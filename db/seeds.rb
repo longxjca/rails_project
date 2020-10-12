076b1b09007f4cf221b43903b37b67715149f063
Longxun Jin <longxjca@gmail.com>
2020-10-11 21:06:24 -0500

    added a csv file for planets

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# require "open-uri"
+# require "json"
+# fetch and decode JSON resources from the Star Wars API
+# def swapi_fetch(url)
+#   JSON.parse(open(url).read)
+# end
+
+# Build the 'person by id' people endpoint URL
+# def person_url(id)
+#   "https://swapi.dev/api/people/#{id}/"
+# end
+
+# character_ids = 1..200
+# character_ids.each do |character_id|
+#   person = swapi_fetch(person_url(character_id))
+#   puts "person name: #{person['name']}"
+# end
+require "csv"
+PersonFilm.destroy_all
+FilmPlanet.destroy_all
+Person.destroy_all
+
+Species.destroy_all
+Planet.destroy_all
+Film.destroy_all
+
+
+
